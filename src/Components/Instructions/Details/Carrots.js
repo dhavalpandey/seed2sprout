520e5da51f11c3ceb0114b6969a6191cba470bb9
Dhaval Pandey <dhavalpandey2007@outlook.com>
2023-02-18 10:55:08 +0000

    carrot seeds image created+uploaded

--- src/Components/Instructions/Details/Carrots.js
+++ src/Components/Instructions/Details/Carrots.js
@@ -2,6 +2,8 @@ import React from "react";
 import GlobalInfo from "./GlobalInfo";
 import "./Details.css";
 import { Helmet } from "react-helmet";
+import CarrotsSeeds from "../../../Images/carrots-seeds.png";
+import FadeIn from "react-fade-in";
 
 export default function Carrots() {
   return (
@@ -11,14 +13,21 @@ export default function Carrots() {
         <span>Everything</span> <span>You</span> <span>Need</span>{" "}
         <span>to</span> <span>Know</span>
       </h1>
-      <div className="details-page">
-        <Helmet>
-          <title>
-            Seed2Sprout • Carrot Seeds 101: Everything You Need to Know
-          </title>
-        </Helmet>
-        <GlobalInfo />
-      </div>
+      <FadeIn>
+        <div className="details-page">
+          <Helmet>
+            <title>
+              Seed2Sprout • Carrot Seeds 101: Everything You Need to Know
+            </title>
+          </Helmet>
+          <GlobalInfo />
+          <div className="card1">
+            <div className="img-container-details">
+              <img src={CarrotsSeeds} alt="carrot-seeds" />
+            </div>
+          </div>
+        </div>
+      </FadeIn>
     </div>
   );
 }
